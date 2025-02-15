d6525c58b1c703f21650535473f74c7afd7ea09a
pvytykac <pvytykac@gmail.com>
2025-02-15 21:22:52 +0100

    added selenium to enable bot prevention bypass

--- src/main/java/net/pvytykac/service/impl/HtmlServiceImpl.java
+++ src/main/java/net/pvytykac/service/impl/HtmlServiceImpl.java
@@ -5,13 +5,10 @@ import net.pvytykac.service.HtmlService;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.springframework.cache.annotation.CacheEvict;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.util.Optional;
 
 /**
@@ -26,27 +23,15 @@ public class HtmlServiceImpl implements HtmlService {
     @Cacheable("html")
     public Html fetchDocument(String url) {
         log.info("fetching html from '{}'", url);
+
+        var driver = new ChromeDriver();
+
         try {
-            var response = Jsoup.connect(url)
-                    .ignoreHttpErrors(true)
-                    .execute();
-
-            if (response.statusCode() != 200) {
-                log.warn("unexpected response from '{}': {} {}", url, response.statusCode(), response.body());
-                throw new IllegalStateException("UnexpectedStatusCode");
-            }
-
-            return new Html(response.parse());
-        } catch (IOException ex) {
-            throw new UncheckedIOException(ex);
+            driver.get(url);
+            return new Html(driver.getPageSource());
+        } finally {
+            driver.quit();
         }
-    }
-
-    //todo: implement actual TTL eviction
-    @CacheEvict(value = "html", allEntries = true)
-    @Scheduled(fixedRate = 30000L)
-    public void clearCache() {
-        log.info("clearing the html cache");
     }
 
     @Slf4j
@@ -54,8 +39,9 @@ public class HtmlServiceImpl implements HtmlService {
 
         private final Document document;
 
-        public Html(Document document) {
-            this.document = document;
+        public Html(String html) {
+            log.debug("{}", html);
+            this.document = Jsoup.parse(html);
         }
 
         public Optional<String> getTextByCss(String selector) {
